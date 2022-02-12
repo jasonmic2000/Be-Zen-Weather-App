99f93b34614b0921e153bf3315f0a451b270d5a3
Jason-desktop <jason_mic2000@yahoo.com>
2022-02-12 10:28:49 +0530

    Implemented solution to prevent duplicate cities

--- app.js
+++ app.js
@@ -6,23 +6,57 @@ const list = document.querySelector(".ajax-section .cities")
 const apiKey = "29ade6445e3addb85d5796b9d0e5ef8c"
 
 form.addEventListener("submit", e => {
-  e.preventDefault()
-  const listItems = list.querySelectorAll(".ajax-section .city")
-  const inputVal = input.value
+  e.preventDefault();
+  let inputVal = input.value;
 
-  // ajax
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`
+  //check if there's already a city
+  const listItems = list.querySelectorAll(".ajax-section .city");
+  const listItemsArray = Array.from(listItems);
+
+  if (listItemsArray.length > 0) {
+    const filteredArray = listItemsArray.filter(el => {
+      let content = "";
+      //athens,gr
+      if (inputVal.includes(",")) {
+        //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
+        if (inputVal.split(",")[1].length > 2) {
+          inputVal = inputVal.split(",")[0];
+          content = el
+            .querySelector(".city-name span")
+            .textContent.toLowerCase();
+        } else {
+          content = el.querySelector(".city-name").dataset.name.toLowerCase();
+        }
+      } else {
+        //athens
+        content = el.querySelector(".city-name span").textContent.toLowerCase();
+      }
+      return content == inputVal.toLowerCase();
+    });
+
+    if (filteredArray.length > 0) {
+      msg.textContent = `You already know the weather for ${
+        filteredArray[0].querySelector(".city-name span").textContent
+      } ...otherwise be more specific by providing the country code as well`;
+      form.reset();
+      input.focus();
+      return;
+    }
+  }
+
+  //ajax here
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      const { main, name, sys, weather } = data
-      const icon = `./icons/${
+      const { main, name, sys, weather } = data;
+      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
         weather[0]["icon"]
-      }.png`
+      }.svg`;
 
-      const li = document.createElement("li")
-      li.classList.add("city")
+      const li = document.createElement("li");
+      li.classList.add("city");
       const markup = `
         <h2 class="city-name" data-name="${name},${sys.country}">
           <span>${name}</span>
@@ -30,18 +64,20 @@ form.addEventListener("submit", e => {
         </h2>
         <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
         <figure>
-          <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+          <img class="city-icon" src="${icon}" alt="${
+        weather[0]["description"]
+      }">
           <figcaption>${weather[0]["description"]}</figcaption>
         </figure>
       `;
-      li.innerHTML = markup
-      list.appendChild(li)
+      li.innerHTML = markup;
+      list.appendChild(li);
     })
     .catch(() => {
       msg.textContent = "Please search for a valid city 😩";
-    })
+    });
 
   msg.textContent = "";
-  form.reset()
-  input.focus()
+  form.reset();
+  input.focus();
 });
