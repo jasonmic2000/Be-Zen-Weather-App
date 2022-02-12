c6b23d544f327e9effac201e4f3b24350b5025f5
Jason-desktop <jason_mic2000@yahoo.com>
2022-02-12 09:34:50 +0530

    Created JS script file and imported API key

---
+++ app.js
@@ -0,0 +1,7 @@
+// Search using city name, or city name, country name
+const form = document.querySelector(".top-banner form")
+const input = document.querySelector(".top-banner input")
+const msg = document.querySelector(".top-banner .msg")
+const list = document.querySelector(".ajax-section .cities")
+
+const apiKey = "29ade6445e3addb85d5796b9d0e5ef8c"
